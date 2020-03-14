17af3bf4f3da3213e1c0a95353e514c836ca391a
kimute <jkdnekki@icloud.com>
2020-03-15 01:54:15 +0900

    テストコード作成(TDD)

---
+++ src/main/java/com_kim/Application.java
@@ -0,0 +1,11 @@
+package com_kim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
